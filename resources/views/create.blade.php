34d03c715ab79de447b6548e789cd23c145057d3
occemper <ravilre22@gmail.com>
2025-04-04 10:37:33 +0300

    018 Kepeeng Old Values in the Form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -19,26 +19,28 @@
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
+
         <div>
             <label for="description">
                 Description
             </label>
-            <textarea type="description" name="description" id="description" rows="5"></textarea>
-            @error('title')
+            <textarea type="description" name="description" id="description" rows="5">{{ old('description') }}</textarea>
+            @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
+
         <div>
             <label for="long_description">
                 Long Description
             </label>
-            <textarea type="long_description" name="long_description" id="long_description" rows="10"></textarea>
-            @error('title')
+            <textarea type="long_description" name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
+            @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
